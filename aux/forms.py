aea3b4e837fff62927f0ce7282a2713c85a91451
Eduardo Ismael <eduardo78d@gmail.com>
2016-07-31 21:23:19 -0500

    forms

---
+++ aux/forms.py
@@ -0,0 +1,9 @@
+from wtforms import Form, BooleanField, StringField, PasswordField, validators, TextField
+from wtforms.fields.html5 import EmailField
+
+class CommentForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25, message='Ingrese un username valido')] )
+    
+    email = EmailField('Correo Electronico', [ validators.Email( message='Ingrese correo electronico valido')])
+
+    comment = TextField('Comentario', [validators.Length(min=5, max=255, message ='El comentario debe contener por lo menos 5 caracteres')])
--- server.py
+++ server.py
@@ -1,9 +1,31 @@
 from flask import Flask
+from flask import request
+from flask import flash
+from flask import redirect
 from flask import render_template
 
-app = Flask(__name__)# template_folder='templates')
+from aux import CommentForm
 
-@app.route('/')
+class Comment:
+	def __init__(self, username, email, comment):
+		self.username = username
+		self.email = email
+		self.comment = comment
+
+	def get_username(self):
+		return self.username
+
+	def get_email(self):
+		return self.email
+
+	def get_comment(self):
+		return self.comment
+
+listComments = []
+app = Flask(__name__)
+app.secret_key = 'some_secret_key'
+
+@app.route('/', methods=['GET'])
 def index():
 	title = "Resort Facilito"
 	hotel_name = "Facilito Resort"
@@ -16,18 +38,16 @@ def index():
 	}
 	return render_template('index.html', context = context)
 
-@app.route('/contact')
-def contact():
-	title = "Contacto Resort Facilito"
-	hotel_name = "Facilito Resort"
-	num_available_room = 400;
+@app.route('/comentario',methods=['GET', 'POST'])
+def comment():
+  form = CommentForm(request.form)
+  if request.method == 'POST' and form.validate():
+  	listComments.append( Comment(form.username.data, form.email.data, form.comment.data) )
+  	flash('Gracias por enviar tu comentario ' + str( len(listComments) ) )
 
-	context = {
-		'title': title,
-		'hotel_name' : hotel_name,
-		'num_available_room' : num_available_room
-	}
-	return render_template('contact.html', context = context)
+  return render_template('comment.html', form=form )
 
 if __name__ == '__main__':
 	app.run()
+
+

ea1584c0b165c020475dc20c2a0a82a2c4128d64
Eduardo Ismael <eduardo78d@gmail.com>
2016-08-20 20:14:55 -0500

    First landing Page

--- aux/forms.py
+++ aux/forms.py
@@ -3,7 +3,8 @@ from wtforms.fields.html5 import EmailField
 
 class CommentForm(Form):
     username = StringField('Username', [validators.Length(min=4, max=25, message='Ingrese un username valido')] )
-    
     email = EmailField('Correo Electronico', [ validators.Email( message='Ingrese correo electronico valido')])
-
     comment = TextField('Comentario', [validators.Length(min=5, max=255, message ='El comentario debe contener por lo menos 5 caracteres')])
+
+class LoginForm(Form):
+	pass
--- server.py
+++ server.py
@@ -1,53 +1,18 @@
 from flask import Flask
-from flask import request
-from flask import flash
-from flask import redirect
 from flask import render_template
 
-from aux import CommentForm
-
-class Comment:
-	def __init__(self, username, email, comment):
-		self.username = username
-		self.email = email
-		self.comment = comment
-
-	def get_username(self):
-		return self.username
-
-	def get_email(self):
-		return self.email
-
-	def get_comment(self):
-		return self.comment
-
-listComments = []
 app = Flask(__name__)
-app.secret_key = 'some_secret_key'
 
 @app.route('/', methods=['GET'])
 def index():
 	title = "Resort Facilito"
-	hotel_name = "Facilito Resort"
-	num_available_room = 400;
-
 	context = {
 		'title': title,
-		'hotel_name' : hotel_name,
-		'num_available_room' : num_available_room
 	}
+
 	return render_template('index.html', context = context)
 
-@app.route('/comentario',methods=['GET', 'POST'])
-def comment():
-  form = CommentForm(request.form)
-  if request.method == 'POST' and form.validate():
-  	listComments.append( Comment(form.username.data, form.email.data, form.comment.data) )
-  	flash('Gracias por enviar tu comentario ' + str( len(listComments) ) )
-
-  return render_template('comment.html', form=form )
-
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True, port=8000)
 
 

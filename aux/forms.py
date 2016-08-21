94d763fd77bc0b49bc2fad1843d3aba3c759e98d
Eduardo Ismael <eduardo78d@gmail.com>
2016-08-20 21:38:59 -0500

    navigation

--- aux/forms.py
+++ aux/forms.py
@@ -1,10 +1,15 @@
-from wtforms import Form, BooleanField, StringField, PasswordField, validators, TextField
+from wtforms import Form, TextAreaField, StringField, PasswordField, validators, TextField
 from wtforms.fields.html5 import EmailField
 
 class CommentForm(Form):
-    username = StringField('Username', [validators.Length(min=4, max=25, message='Ingrese un username valido')] )
-    email = EmailField('Correo Electronico', [ validators.Email( message='Ingrese correo electronico valido')])
-    comment = TextField('Comentario', [validators.Length(min=5, max=255, message ='El comentario debe contener por lo menos 5 caracteres')])
+    username = StringField('Username', 
+    						[validators.Length(min=4, max=25, message='Ingrese un username valido')], 
+    						render_kw={"placeholder": "Username"} )
 
-class LoginForm(Form):
-	pass
+    email = EmailField('Correo Electronico',
+    					[ validators.Email( message='Ingrese correo electronico valido')],
+    					render_kw={"placeholder": "Email"} )
+
+    comment = TextAreaField('Comentario', [validators.Length(min=5, max=255, 
+    					message ='El comentario debe contener por lo menos 5 caracteres')],
+    					render_kw={"placeholder": "Comentario"} )
